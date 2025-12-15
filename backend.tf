4612f129a48bf6ad33b467d278b367f4d424bcb9
Mukul Bhati <mukul@nected.ai>
2025-12-15 17:35:37 +0530

    chore: removed backend unnecessary changes

--- backend.tf
+++ backend.tf
@@ -6,12 +6,3 @@
 #     key                  = "<TFSTATE_FILE_NAME>.tfstate"
 #   }
 # }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "nected-dev"
-    storage_account_name = "tfstate10gpx"
-    container_name       = "tfstate"
-    key                  = "nected.terraform.tfstate"
-  }
-}
