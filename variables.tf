6b379857b460b99d15a1a4e3e964ce5256b87a25
Mukul Bhati <mukul@nected.ai>
2026-06-18 23:00:56 +0530

    chore: bumped up the nected chart version

--- variables.tf
+++ variables.tf
@@ -117,7 +117,7 @@ variable "temporal_chart_version" {
 variable "nected_chart_version" {
   type        = string
   description = "Nected Helm Chart Version"
-  default     = "0.4.45"
+  default     = "0.4.47"
 }
 
 variable "datastore_chart_version" {
